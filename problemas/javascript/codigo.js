2ec5cbd203636f594c4cf0b1303198a918f1c3a6
Estelaraville <elarav2200@alumno.ipn.mx>
2024-04-01 17:25:00 -0600

    1° problmea finzalido

--- problemas/javascript/codigo.js
+++ problemas/javascript/codigo.js
@@ -1,7 +1,24 @@
-function validar(form){
-    if(form.capital.value <=0){
-        alert("Escriba un valor mayor a 0 porfavor");
+function validar(form) {
+    var capital = form.capital.value.trim(); 
+    if (!capital.match(/^(-?\d*\.?\d*)$/)) { 
+        alert("Por favor, ingrese solo números para el capital.");
         form.capital.focus();
         return false;
+    } else if (capital <= 0) {
+        alert("Escriba un valor mayor a 0 por favor.");
+        form.capital.focus();
+        return false;
+    } else {
+        calcularNuevoCapital(capital);
+        return true; 
     }
+}
+
+function calcularNuevoCapital(capital) {
+    var nuevoCapital = parseFloat(capital) * 1.02;
+    mostrarNuevoCapital(nuevoCapital);
+}
+
+function mostrarNuevoCapital(nuevoCapital) {
+    alert("Su capital dentro de un mes será de: "+nuevoCapital.toFixed(2));
 }
